b23d46f262c0eb3f089864ce4abc4b6f6d2145d6
Mark McGregor <mark.mcgregor.contact@gmail.com>
2021-02-04 21:45:18 -0500

    Add ability to pick up the spawning device

--- Pages/Scripts.js
+++ Pages/Scripts.js
@@ -3,12 +3,36 @@ AFRAME.registerComponent('player', {
     isCarryingObject: {type: 'boolean', default: false}
   },
 
-  pickupObject: function() {
-    this.isCarryingObject = true;
-  },
+  init: function() {
+    // get references
+    var self = this;
+    var sceneEl = document.querySelector('a-scene');
 
-  dropObject: function() {
-    this.isCarryingObject = false;
+    this.el.addEventListener('requestPickup', function(event) {
+      if (!this.isCarryingObject) {
+        // pick up the object (fire the pickup event on self)
+        self.el.emit('pickupObject', {objectId: event.detail.requestingObjectId});
+      }
+    });
+
+    this.el.addEventListener('pickupObject', function(event) {
+      // log the pickup
+      console.log("Picked up " + event.detail.objectId);
+
+      // get the new parent and pickupObject
+      var newParent = self.el.querySelector('.holding-point');
+      var pickupObject = document.getElementById(event.detail.objectId);
+
+      // append the pickupObject to the new parent
+      newParent.object3D.add(pickupObject.object3D);
+      pickupObject.setAttribute('position', {x: 0, y: 0, z: 0});
+
+      // activate the object
+      pickupObject.emit('activate');
+
+      // change isCarryingObject to true
+      self.isCarryingObject = true;
+    });
   }
 });
 
@@ -17,20 +41,44 @@ AFRAME.registerComponent('spawning-device', {
     isActive: {type: 'boolean', default: false}
   },
 
-  // Called when clicked, try to pickup the spawning-device
-  tryPickup: {
+  init: function() {
+    // get references
+    var self = this;
+    var button = self.el.querySelector('#button');
+    console.log(button);
 
+    // on spawning-device click
+    this.el.addEventListener('click', function() {
+      console.log(self.isActive);
+      if (self.isActive) {
+        console.log("Button triggered!");
+      } else {
+        document.querySelector('#player').emit('requestPickup', {requestingObjectId: self.el.id});
+      }
+    });
+
+    // on button click
+    button.addEventListener('click', function() {
+      console.log("button clicked");
+
+      if (self.isActive) {
+        // trigger the button
+        console.log("Button triggered!");
+      } else {
+        // trigger the click on the spawning-device
+        self.emit('click');
+        console.log("pass");
+      }
+    });
+
+    this.el.addEventListener('activate', function() {
+      // activate the button
+      self.active = true;
+      console.log("Device activated");
+    });
+
+    this.el.addEventListener('deactivate', function() {
+      self.activate = false;
+    });
   },
-
-  activate: {
-    this.isActive = true;
-
-    console.log("Spawning device activated.")
-  },
-
-  deactivate: {
-    this.isActive = false;
-
-    console.log("Spawning device deactivated.")
-  }
 });
